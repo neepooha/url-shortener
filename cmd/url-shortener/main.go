4428896bf4d9942b29cc29d97374dbe677db3b9f
neepooha <neepooha.1991@gmail.com>
2023-12-21 00:01:52 +0700

    add main.go

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// TODO: init config: cleanenv
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: SQLite
+
+	// TODO: init router: chi
+
+	// TODO: run server
+}
